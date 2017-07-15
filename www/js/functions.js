09fc5136bb65a506127490c5cc4eb2504d422da9
Sebastian <sebastian.b.c.d@gmail.com>
2017-07-15 18:30:16 -0400

    Ventana de inicio

---
+++ www/js/functions.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Seba on 15-07-2017.
+ */
+
+function ingresar(){
+
+}
+
+function registrar(){
+
+}
+
+function guardarRegistro(){
+    var rut = document.getElementById("txtRutRegistroUsuario").value;
+    var nombre = document.getElementById("txtNombreUsuario").value;
+    var direccion = document.getElementById("txtDireccionUsuario").value;
+    var telefono = document.getElementById("txtTelefono").value;
+}
