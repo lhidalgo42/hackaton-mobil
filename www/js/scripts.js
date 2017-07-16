7ad74de8588070f494257503e497d4ad60e41809
Ian Cardenas <iancardenas96@gmail.com>
2017-07-15 20:13:20 -0400

    comit de confirmacion

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,1 +1,9 @@
-// Empty JS for your own code to be here
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".panel-element-615348").hasClass("navbar-collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggle").click();
+        }
+    });
+});
