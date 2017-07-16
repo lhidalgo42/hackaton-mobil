bd2dc8d6a4da1df6ba3ad66dff2362574097b1bf
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2017-07-16 01:28:34 -0400

    se arregla index.html

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,42 +1,67 @@
 function barcoderead() {
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            alert("We got a barcode\n" +
+            $("#result").html("We got a barcode\n" +
                 "Result: " + result.text + "\n" +
                 "Format: " + result.format + "\n" +
                 "Cancelled: " + result.cancelled);
-            //document.getElementById("btnRegistrar").disabled = true;
-            if(result.format=="QR_CODE")
-            {
+
+            if (result.format == "QR_CODE") {
                 $.ajax({
-                    url:result.text,
-                    datatype:'html',
-                    complete:function(data){
-                        document.getElementById("contenidoRegistro").innerHTML=data;
+                    type: "GET",
+                    url: link,
+                    success: function (data) {
+
+
+                        try // Firefox, Mozilla, Opera, etc.
+                        {
+                            parser = new DOMParser();
+                            xmlDoc = parser.parseFromString(data, "text/xml");
+                        }
+                        catch (e) {
+                            alert(e.message);
+                            return;
+                        }
+
+                        var estado = xmlDoc.getElementsByClassName("setWidthOfSecondColumn")[0].innerHTML;
+                        var dni_number = xmlDoc.getElementsByName("form:docNumber")[0].value;
+                        var rut = xmlDoc.getElementsByName("form:run")[0].value;
+                        var tipo = xmlDoc.getElementsByName("form:selectDocType")[0].value;
+                        if(estado == "Vigente"){
+                            if(tipo == "CEDULA") {
+                                $("#txtRut").val(rut);
+                            }
+                            else{
+                                alert("Necesita Tener una Cedula de Identidad Chilena");
+                            }
+                        }
+                        else{
+                            alert("su cedula no esta vigente");
+                        }
                     }
-                })
+                });
 
             }
-            else
-            {
-                alert ("proximamente");
+            else {
+                alert("proximamente");
             }
+
 
         },
         function (error) {
             alert("Scanning failed: " + error);
         },
         {
-            preferFrontCamera : false, // iOS and Android
-            showFlipCameraButton : true, // iOS and Android
-            showTorchButton : false, // iOS and Android
+            preferFrontCamera: false, // iOS and Android
+            showFlipCameraButton: false, // iOS and Android
+            showTorchButton: true, // iOS and Android
             torchOn: false, // Android, launch with the torch switched on (if available)
-            prompt : "Place a barcode inside the scan area", // Android
+            prompt: "Ponga el Carnet de Identidad en la Camara", // Android
             resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-            orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-            disableAnimations : true, // iOS
-            disableSuccessBeep: false // iOS
+            formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations: true, // iOS
+            disableSuccessBeep: true // iOS
         }
     );
 }
